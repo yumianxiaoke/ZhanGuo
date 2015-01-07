88b979b8c5bf58fd73515346cf603240b7519d28
yumianxiaoke <liangxc2014@163.com>
2015-01-08 00:22:49 +0800

    添加动作配置表

---
+++ sg02/Assets/Scripts/XML/Entity/XMLDataGeneralsAnimations.cs
@@ -0,0 +1,14 @@
+
+
+public class XMLDataGeneralsAnimations
+{
+	/// <summary>
+	/// 武将ID,跟武将配置表一致
+	/// </summary>
+	public int ID;
+	/// <summary>
+	/// 攻击1
+	/// </summary>
+	public string[] Animations;
+}
+
---
+++ sg02/Assets/Scripts/XML/Entity/XMLDataWeaponAnimations.cs
@@ -0,0 +1,14 @@
+
+
+public class XMLDataWeaponAnimations
+{
+	/// <summary>
+	/// 对应的是物品ID
+	/// </summary>
+	public int ID;
+	/// <summary>
+	/// 攻击1
+	/// </summary>
+	public string[] Animations;
+}
+
