ca358cde0c4cae3d14f3408a51aeb8544ee066de
ff6330858 <120314439@qq.com>
2015-01-21 10:40:47 +0800

    加了武将列表需要的武将文字显示

--- sg02/Assets/Resources/Lua/UI/SelectGeneralData/SelectGeneralDataView.lua
+++ sg02/Assets/Resources/Lua/UI/SelectGeneralData/SelectGeneralDataView.lua
@@ -5,6 +5,19 @@ m_ButtonsRoot = nil
 function Initialize(viewPanel)
 
     m_MajorListRoot = viewPanel.transform:FindChild("Min_Anchor/MajorList").gameObject
+
+    m_Major = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major").gameObject
+    m_MajorName = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/MajorName").gameObject
+    m_LevelInt = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/LevelInt").gameObject
+    m_StrInt = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/StrInt").gameObject
+    m_IntInt = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/IntInt").gameObject
+    m_VitInt = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/VitInt").gameObject
+    m_SPInt = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/SPInt").gameObject
+    m_MorInt = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/MorInt").gameObject
+    m_ArmsStr = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/ArmsStr").gameObject
+    m_SoldiersInt = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/SoldiersInt").gameObject
+    m_SiteStr = viewPanel.transform:FindChild("Min_Anchor/MajorList/Major/SiteStr").gameObject
+    
     
     m_btMajor = viewPanel.transform:FindChild("Min_Anchor/Major").gameObject
     m_btLevel = viewPanel.transform:FindChild("Min_Anchor/Level").gameObject
